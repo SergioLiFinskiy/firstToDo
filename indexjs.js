852fc4a18acc7b604ddad664bc1bd059b1d53088
SergioLiFinskiy <seregalifinskii@gmail.com>
2023-04-27 00:25:22 +0300

    Change list

--- indexjs.js
+++ indexjs.js
@@ -17,13 +17,14 @@ allNavButton.forEach((button) => {
     })
 })   
 
-
+let toDoLst = [];
 
 const createTaskForm = document.querySelector('.create-task-block');
 createTaskForm.addEventListener('submit', (event) => {
-    // console.log(event);
+
     event.preventDefault();
     const { target } = event;
+    
     const taskNameInput = target.taskName;
     const inputValue = taskNameInput.value;
 
