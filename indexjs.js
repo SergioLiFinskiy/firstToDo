8117d2e55e015d094f77dd27126af1dcb6499e80
SergioLiFinskiy <seregalifinskii@gmail.com>
2023-05-01 13:45:33 +0300

    reality toDo

--- indexjs.js
+++ indexjs.js
@@ -1,148 +1,53 @@
-// const firstNavButton = document.querySelector('.main-navigation__button-item');
-// firstNavButton.addEventListener('click', (event) => {
-//     console.log(event.target);
-//     // const target = event.target;
-//     const {target} = event;
-//     target.classList.add('main-navigation__button-item_selected') 
-// })
+// Find element
+const form = document.querySelector('.create-task-block');
+const taskInput = document.querySelector('.create-task-block__input');
+const taskList = document.querySelector('.tasks-list');
 
-const allNavButton = document.querySelectorAll('.main-navigation__button-item');
-allNavButton.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        allNavButton.forEach((button) => {
-            button.classList.remove('main-navigation__button-item_selected')
-        });
-        const { target } = event;
-        target.classList.add('main-navigation__button-item_selected')
-    })
-})   
+form.addEventListener('submit', addTask);
 
-let toDoLst = [];
+taskList.addEventListener('click', deleteTask);
 
-const createTaskForm = document.querySelector('.create-task-block');
-createTaskForm.addEventListener('submit', (event) => {
+taskList.addEventListener('click', doneTask);
 
+function addTask (event) {
+    // Stop restart
     event.preventDefault();
-    const { target } = event;
+
+    // Take value from form
+    const taskText = taskInput.value;
+
+    // Make task
+    const taskHTML = `<li class="task-item" data-task-id="1">
+                        <div class="task-item__main-container">
+                            <div class="task-item__main-content">
+                                <form class="checkbox-form">
+                                <input class="checkbox-form__checkbox" type="checkbox" id="task-1">
+                                <label for="task-1"></label>
+                                </form>
+                                <span class="task-item__text">${taskText}</span>
+                            </div>
+                            <button data-action = "delete" class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
+                                Удалить
+                            </button>
+                        </div>
+                    </li>`;
     
-    const taskNameInput = target.taskName;
-    const inputValue = taskNameInput.value;
+    // Task in list
+    taskList.insertAdjacentHTML('beforeend',taskHTML);
 
-    const newTask = document.createElement('li');
-    newTask.className = 'task-item';
-    newTask.dataset.id = Date.now();
-    
-    const tastItemMainContainer = document.createElement('div');
-    tastItemMainContainer.className = 'task-item__main-container';
-    newTask.append(tastItemMainContainer);
-
-    const taskItemMainContent = document.createElement('div');
-    taskItemMainContent.className = 'task-item__main-content';
-    tastItemMainContainer.append(taskItemMainContent);
-
-    const checkboxForm = document.createElement('form');
-    checkboxForm.className = 'checkbox-form';
-    taskItemMainContent.append(checkboxForm);
-
-    const inputOfForm = document.createElement('input');
-    inputOfForm.className = 'checkbox-form__checkbox';
-    inputOfForm.type = 'checkbox';
-    inputOfForm.dataset.id = `task-${Date.now()}`;
-    checkboxForm.append(inputOfForm);
-
-    const labelOfForm = document.createElement('label');
-    labelOfForm.className = `task-${Date.now()}`;
-    checkboxForm.append(labelOfForm);
-
-    const spanOfForm = document.createElement('span');
-    spanOfForm.className = 'task-item__text';
-    spanOfForm.textContent = inputValue;
-    taskItemMainContent.append(spanOfForm);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'task-item__delete-button';
-    deleteButton.textContent = 'Удалить';
-    tastItemMainContainer.append(deleteButton);
-
-
-    const taskList = document.querySelector('.tasks-list');
-    taskList.append(newTask);
-
-    // console.log(inputValue);
-    // if (inputValue) {
-    //     alert (`Вы создали задачу ${inputValue}`)
-    // } else {
-    //     alert('Введите правильные данные')
-    // }
-})
-
-const createToolTip = (text) => {
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltip';
-    tooltip.textContent = text;
-
-    return tooltip;
+    // Null of value on start
+    taskInput.value = "";
+    taskInput.focus()
 }
 
-document.addEventListener('mouseover', (event) => {
-    const { target } = event;
-    const mouseOverDeleteBtn = target.className.includes('task-item__delete-button');
-    if (mouseOverDeleteBtn) {
-        const tasItemHTML = target.closest('.task-item');
-        if (tasItemHTML) {
-            const taskDeleteMess = createToolTip('Удалить задачу?');
-            target.append(taskDeleteMess);
-        }
-    }
-})
-
-
-document.addEventListener('mouseout', (event) => {
-    const { target } = event;
-    const mouseOutDeleteBtn = target.className.includes('task-item__delete-button');
-    if (mouseOutDeleteBtn) {
-        const tooltip = document.querySelector('.tooltip');
-        if (tooltip) {
-            tooltip.remove();
-        }
-    }
-})
-
-const taskNameInputOnValidation = (value) => {
-    if(!value || value.includes('@')) {
-        return false
-    }
-    return true
+function deleteTask(event) {
+   if (event.target.dataset.action === "delete") {
+   
+    const parentNote = event.target.closest('.task-item');
+    parentNote.remove()
+   }
 }
 
-const createTaskBlockError = document.querySelector('.create-task-block');
-const taskNameInput = createTaskBlockError.querySelector('.create-task-block__input');
+function doneTask(event) {
 
-taskNameInput.addEventListener('input', (event) => {
-    const { target } = event;
-    const { value } = target;
-    const isValid = taskNameInputOnValidation(value);
-    const messegeBlockFromDOM = document.querySelector('.error-message-block');
-
-    if (!isValid) {
-        const newMessBlock = document.createElement('span');
-        newMessBlock.className = 'error-message-block';
-        newMessBlock.textContent = 'Ошибка ввода';
-        createTaskBlockError.append(newMessBlock)
-    } else if (isValid && messegeBlockFromDOM) {
-        messegeBlockFromDOM.remove()
-    }
-})
-
-
-
-buttonDelete.addEventListener('click', (event) => {
-    const { target } = event;
-    const buttonDelete = document.querySelector(`[data-task-id]=${target}`);
-    if (buttonDelete) {
-        const deleteConfirmed = confirm('Вы уверены?');
-        if(deleteConfirmed) {
-            buttonDelete.remove();
-        }
-    }
-}) 
+}
