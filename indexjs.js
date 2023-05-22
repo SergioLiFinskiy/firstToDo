379b62fe465c61a679b849fe0320f5e71b84717c
SergioLiFinskiy <seregalifinskii@gmail.com>
2023-05-23 00:40:41 +0300

    Ready diary

--- indexjs.js
+++ indexjs.js
@@ -1,8 +1,45 @@
 // Find element
 const form = document.querySelector('.create-task-block');
-const taskInput = document.querySelector('.create-task-block__input');
+const taskInfo = document.querySelector('.task-item__main-content_info');
+const taskInputWords = document.querySelector('#createNewTask');
+const taskInputName = document.querySelector('#selectedContractor');
+const taskInputData = document.querySelector('#createDeadline');
 const taskList = document.querySelector('.tasks-list');
 const allNavButton = document.querySelectorAll('.main-navigation__button-item');
+const checkBox = document.querySelectorAll('.checkbox-form__checkbox');
+
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach((task) => {
+    const taskHTML = `
+    <li class="task-item" id=${task.id}>
+        <div class="task-item__main-container">
+            <div class="task-item__main-content">
+                <form class="checkbox-form">
+                <input class="checkbox-form__checkbox" type="checkbox" id="task-1">
+                <label for="task-1"></label>
+                </form>
+                <div class="task-item__main-content_info">
+                    <div class="task-item__text">${task.text}</div>
+                    <div class="task-item__name">${task.name}</div>
+                    <div class="task-item__data">${task.date}</div>
+                </div>
+                </div>
+            </div>
+            <button data-action = "delete" class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
+                Удалить
+            </button>
+        </div>
+    </li>`
+    
+    // Task in list
+    taskList.insertAdjacentHTML('beforeend',taskHTML);
+})
 
 allNavButton.forEach((button) => {
     button.addEventListener('mouseover', (event) => {
@@ -30,39 +67,65 @@ function addTask (event) {
     event.preventDefault();
 
     // Take value from form
-    const taskText = taskInput.value;
+    const taskText = taskInputWords.value;
+    const taskName = taskInputName.value;
+    const taskData = taskInputData.value;
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        name: taskName,
+        date: taskData,
+        done: false,
+    };
+    
+    tasks.push(newTask);
+
+    saveToLocalStorage();
 
     // Make task
-    const taskHTML = `<li class="task-item" data-task-id="1">
-                        <div class="task-item__main-container">
-                            <div class="task-item__main-content">
-                                <form class="checkbox-form">
-                                <input class="checkbox-form__checkbox" type="checkbox" id="task-1">
-                                <label for="task-1"></label>
-                                </form>
-                                <span class="task-item__text">${taskText}</span>
-                            </div>
-                            <button data-action = "delete" class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
-                                Удалить
-                            </button>
-                        </div>
-                    </li>`;
+    const taskHTML = `
+    <li class="task-item" id=${newTask.id}>
+        <div class="task-item__main-container">
+            <div class="task-item__main-content">
+                <form class="checkbox-form">
+                <input class="checkbox-form__checkbox" type="checkbox" id="task-1">
+                <label for="task-1"></label>
+                </form>
+                <div class="task-item__main-content_info">
+                    <div class="task-item__text">${newTask.text}</div>
+                    <div class="task-item__name">${newTask.name}</div>
+                    <div class="task-item__data">${newTask.date}</div>
+                </div>
+                </div>
+            </div>
+            <button data-action = "delete" class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
+                Удалить
+            </button>
+        </div>
+    </li>`
     
     // Task in list
     taskList.insertAdjacentHTML('beforeend',taskHTML);
 
     // Null of value on start
-    taskInput.value = "";
-    taskInput.focus()
+    taskInputWords.value = "";
+    taskInputName.value = "";
+    taskInputData.value = "";
+    taskInputWords.focus()
 }
 
 function deleteTask(event) {
    if (event.target.dataset.action === "delete") {
     const parentNote = event.target.closest('.task-item');
+
+    const id = Number(parentNote.id); 
+    tasks = tasks.filter((task) => task.id !== id)
+    saveToLocalStorage();
     parentNote.remove()
    }
 }
 
-function doneTask(event) {
-
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
