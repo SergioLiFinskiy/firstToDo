98e2336b254d2c18780a5a60da53cecdc839ddc3
SergioLiFinskiy <seregalifinskii@gmail.com>
2023-06-05 00:08:38 +0300

    take sort of name

--- indexjs.js
+++ indexjs.js
@@ -11,19 +11,109 @@ const   form = document.querySelector('.create-task-block'),
         taskInputWords = document.querySelector('#createNewTask'),
         taskInputName = document.querySelector('#selectedContractor'),
         taskInputData = document.querySelector('#createDeadline'),
-        taskList = document.querySelector('.tasks-list'),
+        taskList = document.querySelector('.task-list_panel'),
         allNavButton = document.querySelectorAll('.main-navigation__button-item'),
-        checkBox = document.querySelectorAll('.checkbox-form__checkbox');
-        
+        checkBox = document.querySelectorAll('.checkbox-form__checkbox'),
+        select = document.querySelector('#sortName'),
+        taskItem = document.querySelectorAll('.task-item');
+
 let tasks = [];
 let taskSortOfName = [];
 let taskSortUniq = [];
 
 if (localStorage.getItem('tasks')){
     tasks = JSON.parse(localStorage.getItem('tasks'));
+    taskSortOfName = JSON.parse(localStorage.getItem('name'));
+    taskSortUniq = JSON.parse(localStorage.getItem('sortName'));
 }
 
 tasks.forEach((task) => {
+    renderTask (task);
+    console.log(taskSortUniq);
+})
+
+allNavButton.forEach((button) => {
+    button.addEventListener('mouseover', (event) => {
+        allNavButton.forEach((button) => {
+            button.classList.remove('main-navigation__button-item_selected')
+        });
+        const { target } = event;
+        target.classList.add('main-navigation__button-item_selected');
+    })
+
+    button.addEventListener('mouseout', (event) => {
+        const { target } = event;
+        target.classList.remove('main-navigation__button-item_selected');    
+    })
+}) 
+
+form.addEventListener('submit', addTask);
+
+taskList.addEventListener('click', deleteTask);
+
+select.addEventListener('click', sortOfName);
+
+function addTask (event) {
+    // Stop restart
+    event.preventDefault();
+
+    // Take value from form
+    const taskText = taskInputWords.value;
+    const taskName = taskInputName.value;
+    const taskData = taskInputData.value;
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        name: taskName,
+        date: taskData,
+        done: false,
+    };
+
+    tasks.push(newTask);
+    taskSortOfName.push(newTask.name);
+    taskSortUniq = Array.from(new Set(taskSortOfName));
+
+    saveToLocalStorage();
+
+    // Make task
+    renderTask(newTask);
+
+    // Null of value on start
+    taskInputWords.value = "";
+    taskInputName.value = "";
+    taskInputData.value = "";
+    taskInputWords.focus()
+}
+
+function deleteTask(event) {
+   if (event.target.dataset.action === "delete") {
+    const parentNote = event.target.closest('.task-item');
+    const id = Number(parentNote.id); 
+    const name = parentNote.name; 
+    tasks = tasks.filter((task) => task.id !== id);
+    taskSortOfName = taskSortOfName.filter((task) => task.name !== id);
+    taskSortUniq = taskSortUniq.filter((task) => task.name !== id);
+
+    // clearPositionOfLocalStorage(name);
+
+    saveToLocalStorage();
+    parentNote.remove();
+   }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('name', JSON.stringify(taskSortOfName));
+    localStorage.setItem('sortName', JSON.stringify(taskSortUniq));
+}
+
+// function clearPositionOfLocalStorage() {
+//     localStorage.removeItem('name', JSON.stringify(taskSortOfName));
+//     localStorage.removeItem('sortName', JSON.stringify(taskSortUniq));
+// }
+
+function renderTask (task) {
     const taskHTML = `
     <li class="task-item" id=${task.id}>
         <div class="task-item__main-container">
@@ -47,104 +137,16 @@ tasks.forEach((task) => {
     
     // Task in list
     taskList.insertAdjacentHTML('beforeend',taskHTML);
-})
+}
 
-allNavButton.forEach((button) => {
-    button.addEventListener('mouseover', (event) => {
-        allNavButton.forEach((button) => {
-            button.classList.remove('main-navigation__button-item_selected')
-        });
-        const { target } = event;
-        target.classList.add('main-navigation__button-item_selected');
-    })
-
-    button.addEventListener('mouseout', (event) => {
-        const { target } = event;
-        target.classList.remove('main-navigation__button-item_selected');    
-    })
-}) 
-
-form.addEventListener('submit', addTask);
-
-taskList.addEventListener('click', deleteTask);
-
-function addTask (event) {
-    // Stop restart
+function sortOfName(event) {
     event.preventDefault();
-
-    // Take value from form
-    const taskText = taskInputWords.value;
-    const taskName = taskInputName.value;
-    const taskData = taskInputData.value;
-
-    const newTask = {
-        id: Date.now(),
-        text: taskText,
-        name: taskName,
-        date: taskData,
-        done: false,
-    };
-    
-    tasks.push(newTask);
-    taskSortOfName.push(newTask.name);
-    taskSortUniq = Array.from(new Set(taskSortOfName));
-
-    saveToLocalStorage();
-
-    // Make task
-    const taskHTML = `
-    <li class="task-item" id=${newTask.id}>
-        <div class="task-item__main-container">
-            <div class="task-item__main-content">
-                <form class="checkbox-form">
-                <input class="checkbox-form__checkbox" type="checkbox" id="task-1">
-                <label for="task-1"></label>
-                </form>
-                <div class="task-item__main-content_info">
-                    <div class="task-item__text">${newTask.text}</div>
-                    <div class="task-item__name">${newTask.name}</div>
-                    <div class="task-item__data">${newTask.date}</div>
-                </div>
-                </div>
-            </div>
-            <button data-action = "delete" class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
-                Удалить
-            </button>
-        </div>
-    </li>`
-    
-    // Task in list
-    taskList.insertAdjacentHTML('beforeend',taskHTML);
-
-    // Null of value on start
-    taskInputWords.value = "";
-    taskInputName.value = "";
-    taskInputData.value = "";
-    taskInputWords.focus()
-}
-
-function deleteTask(event) {
-   if (event.target.dataset.action === "delete") {
-    const parentNote = event.target.closest('.task-item');
-
-    const id = Number(parentNote.id); 
-    tasks = tasks.filter((task) => task.id !== id)
-    saveToLocalStorage();
-    parentNote.remove()
-   }
-}
-
-function saveToLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
-
-function sortOfName() {
-    for (let i = 0; i <= taskSortUniq.length; i++) {
+    // select.innerHTML = '';
+    for (let i = 0; i < taskSortUniq.length; i++) {
         let optn = taskSortUniq[i];
-        let el = document.createElement("option");
-        let select = document.querySelector('#sortName');
+        let el = document.createElement("option");     
         el.textContent = optn;
         el.value = optn;
         select.appendChild(el);
-    } 
+    }
 }
